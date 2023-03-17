36e9a8638de750fc4ae74be17cce1d47325462ee
sormys <szymonp1806@gmail.com>
2023-03-17 21:01:23 +0100

    Added prettier.

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,8 +1,8 @@
-import {port} from '../index'
+import { port } from '../index'
 
 test('Simple test', async () => {
-        // for some reason using port from imported varaible gives weird errors
-	let response = await fetch('http://localhost:42069/test')
-	let text = await response.text();
-	expect(text).toEqual('Hello from typescript express!')
-});
+  // for some reason using port from imported varaible gives weird errors
+  let response = await fetch('http://localhost:42069/test')
+  let text = await response.text()
+  expect(text).toEqual('Hello from typescript express!')
+})
