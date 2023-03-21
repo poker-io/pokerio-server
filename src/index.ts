5ba1031c76cb476ad795b8e8a00abb9416989a5f
sormys <szymonp1806@gmail.com>
2023-03-21 22:16:54 +0100

    Changed way of  authorizing via a service account.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express from 'express'
 import admin from 'firebase-admin'
+import { readFileSync } from 'fs'
+
+const serviceAccount = JSON.parse(readFileSync('./src/serviceAccount.json', 'utf-8'))
 
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert({
+    privateKey: serviceAccount.private_key,
+    clientEmail: serviceAccount.client_email,
+    projectId: serviceAccount.project_id,
+  }),
 })
 
 const app = express()
