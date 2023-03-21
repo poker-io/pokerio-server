5f79090a9d62333445e554aa21b47d81e135f606
Karol Wąsowski <wasowski02@protonmail.com>
2023-03-21 12:06:36 +0100

    Added init to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { databaseInit } from './databaseConnection'
 
 const app = express()
 export const port = 42069
@@ -7,6 +8,14 @@ app.get('/test', (req, res) => {
   res.send('Hello from typescript express!')
 })
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at localhost:${port}`)
-})
+databaseInit()
+  .then(() => {
+    // Only start listening if database connection was successful
+    app.listen(port, () => {
+      console.log(`[server]: Server is running at localhost:${port}`)
+    })
+  })
+  .catch(() => {
+    // Don't start the server in case of an error
+    console.log('Failed to init database')
+  })
