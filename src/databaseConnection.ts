4d6dbbdb5b811a20e25ae872e8b27ebe40b820e3
Karol Wąsowski <wasowski02@protonmail.com>
2023-03-23 18:14:17 +0100

    Database setup (#25)
    Some setup is required.
    
    1. Add a file `src/secrets.ts` with the contents:
    ```ts
    export const user = '<your username>'
    export const password = '<your password>'
    ```
    2. Before running `yarn start` setup tunneling with:
    ```
    ssh -L 5432:lkdb:5432 ab123456@students.mimuw.edu.pl
    ```
    3. Run `yarn start`
    
    If everything worked correctly, you should see two tables created under
    your account: `Games` and `Players`.
    
    Closes #4

---
+++ src/databaseConnection.ts
@@ -0,0 +1,78 @@
+import pg from 'pg'
+import { user, password } from './secrets'
+// pq is a CommonJS module, so we have to do it this way for the import to work
+const { Client } = pg
+
+const client = new Client({
+  user,
+  password,
+  database: 'bd',
+  port: 5432,
+  host: 'localhost',
+})
+
+export async function databaseInit(): Promise<void> {
+  let success = true
+
+  try {
+    // Connect
+    await client.connect()
+
+    // Create the tables
+    // This will fail if tables already exist, but we don't care
+    try {
+      await client.query(
+        `CREATE TABLE IF NOT EXISTS Players (
+          id SERIAL UNIQUE NOT NULL,
+          token VARCHAR(250) NOT NULL PRIMARY KEY,
+          nickname VARCHAR(20) NOT NULL,
+          turn BIGINT NOT NULL,
+          game_id VARCHAR(6),
+          card1 VARCHAR(3),
+          card2 VARCHAR(3),
+          funds BIGINT,
+          bet BIGINT
+        )`
+      )
+    } catch (err) {
+      console.error(err)
+    }
+
+    try {
+      await client.query(
+        `CREATE TABLE IF NOT EXISTS Games (
+          game_id VARCHAR(6) PRIMARY KEY,
+          game_master SERIAL REFERENCES Players(id) NOT NULL,
+          card1 VARCHAR(3),
+          card2 VARCHAR(3),
+          card3 VARCHAR(3),
+          card4 VARCHAR(3),
+          card5 VARCHAR(3),
+          game_round BIGINT DEFAULT 0 NOT NULL,
+          starting_funds BIGINT NOT NULL,
+          small_blind BIGINT NOT NULL,
+          small_blind_who SERIAL REFERENCES Players(id) NOT NULL,
+          current_table_value BIGINT,
+          current_player SERIAL REFERENCES Players(id) NOT NULL
+        )`
+      )
+    } catch (err) {
+      console.error(err)
+    }
+  } catch (err) {
+    console.error(err)
+    success = false
+  } finally {
+    try {
+      await client.end()
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  if (success) {
+    await Promise.resolve()
+  } else {
+    await Promise.reject(new Error('Failed to connect to database'))
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express from 'express'
+import { app, port } from './app.js'
+import { databaseInit } from './databaseConnection'
 import admin from 'firebase-admin'
 import { readFileSync } from 'fs'
 
@@ -14,13 +15,15 @@ admin.initializeApp({
   }),
 })
 
-const app = express()
-export const port = 42069
-
-app.get('/test', (req, res) => {
-  res.send('Hello from typescript express!')
-})
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at localhost:${port}`)
-})
+databaseInit()
+  .then(() => {
+    // Only start listening if database connection was successful
+    app.listen(port, () => {
+      console.log(`[server]: Server is running at localhost:${port}`)
+    })
+  })
+  .catch(() => {
+    // Don't start the server in case of an error
+    console.log('Failed to init database')
+    process.exit(1)
+  })
