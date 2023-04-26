aa3994ade5032e52f026f45d83729b15818b9060
Mikołaj Piróg <69601940+aetn23@users.noreply.github.com>
2023-04-26 11:46:34 +0200

    Split functions from app.ts into routers (#30)

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -1,16 +1,6 @@
 import { app } from '../app'
 import request from 'supertest'
-import { databaseInit } from '../databaseConnection'
 
-test('Simple test', (done) => {
-  request(app)
-    .get('/test')
-    .expect(200)
-    .expect('Content-Type', 'text/html; charset=utf-8')
-    .expect('Hello from typescript express!')
-    .end(done)
-})
-
-test('Database connection', async () => {
-  await expect(databaseInit()).resolves.not.toThrow()
+test('Status test', (done) => {
+  request(app).get('/status').expect(200).end(done)
 })
