a92d73255bb095123192080064b780bd2c5b5684
aetn23 <mikolajpirog@gmail.com>
2023-04-26 17:14:52 +0200

    Fix query

--- src/routes/kickPlayer.ts
+++ src/routes/kickPlayer.ts
@@ -35,7 +35,7 @@ router.get(
         const getGameQuery = 'SELECT game_id FROM Games WHERE game_master=$1'
         const verifyPlayerInGameQuery = 'SELECT token FROM Players WHERE token=$1 AND game_id=$2'
         const removePlayerFromGameQuery = 'DELETE FROM Players WHERE token=$1'
-        const getNewSmallBlindCurrentPlayer = 'SELECT token FROM Players WHERE turn IN (SELECT MIN(turn) FROM Players WHERE token!=$1 AND game_id=$2) LIMIT 1'
+        const getNewSmallBlindCurrentPlayer = 'SELECT token FROM Players WHERE turn IN (SELECT MIN(turn) FROM Players WHERE token!=$1 ) AND game_id=$2 LIMIT 1'
         const setNewSmallBlindCurrentPlayerQuery = 'UPDATE Games SET small_blind_who=$1, current_player=$2 WHERE game_id=$3'
 
         await client.query(getGameQuery, [req.query.creatorToken]).then(async (getGameRes) => {
