poker-io/pokerio-server

commit 0c31500f9a5c5faf08c1358b4d45e09fd2d9577f

Different error code for every error (#61)
This PR fixes issue with repeating error code 400 (one from celebrate
and one from some other error) in each request. Tests has been updated
to request new error codes.

src/routes/gameplay/fold.ts
@@ -37,11 +37,11 @@ router.get(
 
     await runRequestWithClient(res, async (client) => {
       if (!(await isPlayerInGame(playerToken, gameId, client))) {
-        return res.sendStatus(400)
+        return res.sendStatus(402)
       }
 
       if (!(await isPlayersTurn(playerToken, gameId, client))) {
-        return res.sendStatus(402)
+        return res.sendStatus(403)
       }
       await setPlayerState(playerToken, client, PlayerState.Folded)
       const newPlayer = await setNewCurrentPlayer(playerToken, gameId, client)

src/tests/actionRaise.test.ts
@@ -58,7 +58,7 @@ test('Raise, correct arguments 1', async () => {
       .get(
         `/actionRaise?playerToken=${players[1].token}&gameId=${gameId}&amount=5`
       )
-      .expect(402)
+      .expect(403)
 
     await request(app)
       .get(
@@ -81,13 +81,13 @@ test('Raise, correct arguments 1', async () => {
       .get(
         `/actionRaise?playerToken=${players[2].token}&gameId=${gameId}&amount=2`
       )
-      .expect(404)
+      .expect(405)
 
     await request(app)
       .get(
         `/actionRaise?playerToken=${players[2].token}&gameId=${gameId}&amount=2000000000`
       )
-      .expect(403)
+      .expect(404)
 
     await request(app)
       .get(

src/tests/call.test.ts
@@ -45,7 +45,7 @@ test('Call, correct arguments 1', async () => {
       .get(
         `/actionRaise?playerToken=${players[1].token}&gameId=${gameId}&amount=5`
       )
-      .expect(402)
+      .expect(403)
 
     await request(app)
       .get(
@@ -67,13 +67,13 @@ test('Call, correct arguments 1', async () => {
       .get(
         `/actionRaise?playerToken=${players[2].token}&gameId=${gameId}&amount=2`
       )
-      .expect(404)
+      .expect(405)
 
     await request(app)
       .get(
         `/actionRaise?playerToken=${players[2].token}&gameId=${gameId}&amount=2000000000`
       )
-      .expect(403)
+      .expect(404)
 
     await request(app)
       .get(

src/tests/check.test.ts
@@ -49,10 +49,10 @@ test('Check, wrong args', async () => {
     const players = await getPlayersInGame(gameId, client)
     await request(app)
       .get(`/actionCheck?playerToken=${players[1].token}&gameId=${gameId}`)
-      .expect(402)
+      .expect(403)
     await request(app)
       .get(`/actionCheck?playerToken=${players[0].token}&gameId=${gameId}`)
-      .expect(403)
+      .expect(404)
   })
 }, 20000)
 

src/tests/fold.test.ts
@@ -59,7 +59,7 @@ test('Fold, correct arguments, wrong turn', async () => {
     const players = await getPlayersInGame(gameId, client)
     await request(app)
       .get(`/actionFold?playerToken=${players[1].token}&gameId=${gameId}`)
-      .expect(402)
+      .expect(403)
 
     await request(app)
       .get(`/actionFold?playerToken=${players[0].token}&gameId=${gameId}`)

src/tests/kickPlayer.test.ts
@@ -39,7 +39,7 @@ test('Kick player, correct arguments', async () => {
         .concat(gameMasterToken)
         .concat('&playerToken=2137')
     )
-    .expect(402)
+    .expect(403)
 
   const verifyNoPlayerQuery = 'SELECT token FROM Players WHERE token=$1'
 

src/routes/createGame.ts
@@ -42,7 +42,7 @@ router.get(
 
     await runRequestWithClient(res, async (client) => {
       if (await isPlayerInAnyGame(creator.token, client)) {
-        return res.sendStatus(400)
+        return res.sendStatus(402)
       }
 
       await createPlayer(creator, null, client)

src/routes/gameplay/call.ts
@@ -39,17 +39,17 @@ router.get(
 
     await runRequestWithClient(res, async (client) => {
       if (!(await isPlayerInGame(playerToken, gameId, client))) {
-        return res.sendStatus(400)
+        return res.sendStatus(402)
       }
 
       if (!(await isPlayersTurn(playerToken, gameId, client))) {
-        return res.sendStatus(402)
+        return res.sendStatus(403)
       }
 
       const maxBet = await getMaxBet(gameId, client)
 
       if (!(await playerHasEnoughMoney(gameId, playerToken, maxBet, client))) {
-        return res.sendStatus(403)
+        return res.sendStatus(404)
       }
 
       const newPlayer = await setNewCurrentPlayer(playerToken, gameId, client)

commit fc715a0420410cd3efe03b0fa27876a4b08eb140

Adding more tests (#62)
This PR is using error codes introduced in #61.

- added more tests for requests
- moved calculateWinner tests to separate file
- changed how first player of new round is chosen 
- fixed checking if the round should change

src/routes/gameplay/fold.ts
@@ -12,6 +12,7 @@ import {
   setPlayerState,
   setNewCurrentPlayer,
   changeGameRoundIfNeeded,
+  playersStillInGame,
 } from '../../utils/commonRequest'
 import sha256 from 'crypto-js/sha256'
 import { PlayerState } from '../../utils/types'
@@ -78,11 +79,3 @@ router.get(
 )
 
 export default router
-
-export async function playersStillInGame(gameId: string, client) {
-  const query = `SELECT token
-  FROM players
-  WHERE game_id = $1 AND (last_action <> $2 OR last_action IS NULL)`
-  const values = [gameId, PlayerState.Folded]
-  return (await client.query(query, values)).rows
-}

src/utils/commonRequest.ts
@@ -90,8 +90,6 @@ export async function setNewCurrentPlayer(
 
   const getPlayersAndTurn =
     'SELECT token, turn, last_action FROM Players WHERE game_id=$1 AND (last_action IS NULL OR last_action<>$2) ORDER BY turn ASC'
-  const setNewCurrentPlayer =
-    'UPDATE Games SET current_player=$1 WHERE game_id=$2'
 
   const oldTurn = await (
     await client.query(getOldPlayerTurn, [oldPlayerToken])
@@ -105,18 +103,12 @@ export async function setNewCurrentPlayer(
   } else {
     for (let i = 0; i < playersTurns.rowCount; i++) {
       if (playersTurns.rows[i].turn > oldTurn) {
-        await client.query(setNewCurrentPlayer, [
-          playersTurns.rows[i].token,
-          gameId,
-        ])
+        await setCurrentPlayer(gameId, playersTurns.rows[i].token, client)
         return playersTurns.rows[i].token
       }
     }
 
-    await client.query(setNewCurrentPlayer, [
-      playersTurns.rows[0].token,
-      gameId,
-    ])
+    await setCurrentPlayer(gameId, playersTurns.rows[0].token, client)
 
     return playersTurns.rows[0].token
   }
@@ -130,31 +122,60 @@ export async function changeGameRoundIfNeeded(
   // The next round commences only if there is one active player OR when current player was the last raiser
   const shouldProceedNextRound = `SELECT 1 FROM Players A WHERE 
     (A.token=$1 AND A.last_action=$2 AND 1 = 
-        (SELECT COUNT(*) FROM Players B WHERE B.last_action=$2)) OR 
-            (SELECT COUNT(*) FROM Players C WHERE (C.last_action=$3 
-            OR (C.bet=0 AND C.funds=0))) = $4`
+        (SELECT COUNT(*) FROM Players B WHERE B.last_action=$2 AND B.game_id=$3)) OR 
+            (SELECT COUNT(*) FROM Players C WHERE (C.last_action=$4 
+            OR (C.bet=0 AND C.funds=0)) AND game_id=$3) = $5`
   const playerCount = (await getPlayersInGame(gameId, client)).length
   const updateGameRound =
     'UPDATE Games SET game_round=game_round + 1 WHERE game_id=$1'
-  const setFirstPlayer =
-    'UPDATE Games SET current_player=(SELECT token FROM Players WHERE turn=0 AND game_id=$1) WHERE game_id=$1'
   if (
     (
       await client.query(shouldProceedNextRound, [
         currentPlayerToken,
         PlayerState.Raised,
+        gameId,
         PlayerState.Folded,
         playerCount - 1,
       ])
     ).rowCount !== 0
   ) {
     await client.query(updateGameRound, [gameId])
-    await client.query(setFirstPlayer, [gameId])
+    const startingPlayer = await chooseRoundStartingPlayer(gameId, client)
+    await setCurrentPlayer(gameId, startingPlayer, client)
+
     // todo count cards and set winners
     return true
   } else {
     return false
   }
+}
+
+export async function chooseRoundStartingPlayer(
+  gameId: string,
+  client: PoolClient
+): Promise<string> {
+  const players = await playersStillInGame(gameId, client)
+  const bigBlindTurn = await getMaxTurn(gameId, client)
+  players.sort((a, b) => b.turn - a.turn)
+  // If small blind or big blind is still in game, they start the round.
+  return players[0].turn >= bigBlindTurn - 1
+    ? players[0].token
+    : players[players.length - 1].token
+}
+
+export async function getMaxTurn(gameId: string, client: PoolClient) {
+  const query = 'SELECT MAX(turn) as mt FROM Players WHERE game_id=$1'
+  return (await client.query(query, [gameId])).rows[0].mt
+}
+
+export async function setCurrentPlayer(
+  gameId: string,
+  playerToken: string,
+  client: PoolClient
+) {
+  const query = 'UPDATE games SET current_player=$1 where game_id=$2'
+  const values = [playerToken, gameId]
+  await client.query(query, values)
 }
 
 export async function getPlayersInGame(
@@ -310,4 +331,12 @@ export async function getMaxBet(
 ): Promise<string> {
   const query = 'SELECT MAX(bet) as max FROM Players WHERE game_id=$1'
   return (await client.query(query, [gameId])).rows[0].max
+}
+
+export async function playersStillInGame(gameId: string, client) {
+  const query = `SELECT token, turn
+  FROM players
+  WHERE game_id = $1 AND (last_action <> $2 OR last_action IS NULL)`
+  const values = [gameId, PlayerState.Folded]
+  return (await client.query(query, values)).rows
 }

src/tests/actionRaise.test.ts
@@ -8,6 +8,7 @@ import './testSuiteTeardown'
 test('Raise, wrong args', (done) => {
   const wrongToken = 'TESTRAISE_INCORRECT'
   const wrongGameId = 'WRONG_ID'
+  const correctId = '1'
   request(app)
     .get(`/actionRaise?playerToken=${wrongToken}`)
     .expect(400)
@@ -16,6 +17,10 @@ test('Raise, wrong args', (done) => {
   request(app)
     .get(`/actionRaise?playerToken=${wrongToken}&gameId=${wrongGameId}`)
     .expect(400)
+    .end(done)
+  request(app)
+    .get(`/actionRaise?playerToken=${wrongToken}&gameId=${correctId}&amount=50`)
+    .expect(402)
     .end(done)
 })
 

src/tests/calculatingWinner.test.ts
@@ -1,0 +1,106 @@
+import { app } from '../app'
+import request from 'supertest'
+import { runRequestWithClient } from '../utils/databaseConnection'
+import { PlayerState, type NewGameInfo } from '../utils/types'
+import {
+  calculateWinner,
+  getGameIdAndStatus,
+  getPlayersInGame,
+} from '../utils/commonRequest'
+import { convertCardName } from '../utils/randomise'
+import './testSuiteTeardown'
+
+test('calculate winner', async () => {
+  const gameMasterToken = 'CALCUlATETEST'
+  const gameMasterNick = 'CALCUlATENICK'
+  const playerToken = 'CALCUlATETEST2'
+  const playerNick = 'CALCUlATENICK2'
+  const player2Token = 'CALCUlATETEST3'
+  const player2Nick = 'CALCUlATENICK3'
+
+  await runRequestWithClient(undefined, async (client) => {
+    const res = await request(app)
+      .get(
+        `/createGame?creatorToken=${gameMasterToken}&nickname=${gameMasterNick}`
+      )
+      .expect(200)
+
+    const key = (res.body as NewGameInfo).gameId
+    await request(app)
+      .get(
+        `/joinGame?playerToken=${playerToken}&nickname=${playerNick}&gameId=${key}`
+      )
+      .expect(200)
+
+    await request(app)
+      .get(
+        `/joinGame?playerToken=${player2Token}&nickname=${player2Nick}&gameId=${key}`
+      )
+      .expect(200)
+
+    await request(app)
+      .get(`/startGame?creatorToken=${gameMasterToken}`)
+      .expect(200)
+
+    const gameId =
+      (await getGameIdAndStatus(gameMasterToken, client)).gameId ?? ''
+    const players = await getPlayersInGame(gameId, client)
+
+    await client.query('UPDATE players SET last_action=$1 WHERE game_id=$2', [
+      PlayerState.Called,
+      gameId,
+    ])
+    await request(app)
+      .get(`/actionFold?playerToken=${players[0].token}&gameId=${gameId}`)
+      .expect(200)
+    await client.query('UPDATE players SET CARD1=$1, CARD2=$2 WHERE token=$3', [
+      '01K',
+      '01O',
+      players[1].token,
+    ])
+    await client.query('UPDATE players SET CARD1=$1, CARD2=$2 WHERE token=$3', [
+      '13K',
+      '13O',
+      players[2].token,
+    ])
+    await client.query(
+      'UPDATE games SET CARD1=$1, CARD2=$2, CARD3=$3, CARD4=$4, CARD5=$5  WHERE game_id=$6',
+      ['02K', '03O', '05K', '07O', '08P', gameId]
+    )
+    let calculateResult = await calculateWinner(gameId, client)
+    expect(calculateResult.includes(players[1].token)).toBe(true)
+    expect(calculateResult.includes(players[2].token)).toBe(false)
+    expect(calculateResult.includes(players[0].token)).toBe(false)
+    await client.query('UPDATE players SET CARD1=$1, CARD2=$2 WHERE token=$3', [
+      '01P',
+      '01T',
+      players[2].token,
+    ])
+    calculateResult = await calculateWinner(gameId, client)
+    expect(calculateResult.includes(players[1].token)).toBe(true)
+    expect(calculateResult.includes(players[2].token)).toBe(true)
+    expect(calculateResult.includes(players[0].token)).toBe(false)
+  })
+}, 20000)
+
+test('card conversion test', async () => {
+  expect(convertCardName('01K')).toEqual('Ah')
+  expect(convertCardName('02K')).toEqual('2h')
+  expect(convertCardName('03K')).toEqual('3h')
+  expect(convertCardName('04K')).toEqual('4h')
+  expect(convertCardName('05K')).toEqual('5h')
+  expect(convertCardName('06K')).toEqual('6h')
+  expect(convertCardName('07K')).toEqual('7h')
+  expect(convertCardName('08K')).toEqual('8h')
+  expect(convertCardName('09K')).toEqual('9h')
+  expect(convertCardName('10K')).toEqual('Th')
+  expect(convertCardName('11K')).toEqual('Jh')
+  expect(convertCardName('12K')).toEqual('Qh')
+  expect(convertCardName('13K')).toEqual('Kh')
+  expect(convertCardName('01O')).toEqual('Ad')
+  expect(convertCardName('02O')).toEqual('2d')
+  expect(convertCardName('03P')).toEqual('3s')
+  expect(convertCardName('11P')).toEqual('Js')
+  expect(convertCardName('12T')).toEqual('Qc')
+  expect(convertCardName('04T')).toEqual('4c')
+})

src/tests/call.test.ts
@@ -2,8 +2,22 @@ import { app } from '../app'
 import request from 'supertest'
 import { runRequestWithClient } from '../utils/databaseConnection'
 import type { NewGameInfo } from '../utils/types'
-import { getGameIdAndStatus, getPlayersInGame } from '../utils/commonRequest'
+import {
+  STARTING_FUNDS_DEFAULT,
+  getGameIdAndStatus,
+  getPlayersInGame,
+} from '../utils/commonRequest'
 import './testSuiteTeardown'
+
+test('Call, wrong args', (done) => {
+  request(app).get('/actionCall').expect(400).end(done)
+  request(app).get('/actionCall?playerToken=2137').expect(400).end(done)
+  request(app).get('/actionCall?gameId=2137').expect(400).end(done)
+  request(app)
+    .get('/actionCall?playerToken=2137&gameId=2137')
+    .expect(402)
+    .end(done)
+})
 
 test('Call, correct arguments 1', async () => {
   const gameMasterToken = 'CALLTEST'
@@ -41,6 +55,24 @@ test('Call, correct arguments 1', async () => {
       (await getGameIdAndStatus(gameMasterToken, client)).gameId ?? ''
     const players = await getPlayersInGame(gameId, client)
 
+    // not his turn
+    await request(app)
+      .get(`/actionCall?playerToken=${players[1].token}&gameId=${gameId}`)
+      .expect(403)
+
+    const takeAllMoneyQuery = 'UPDATE Players SET funds=10 WHERE token=$1'
+    await client.query(takeAllMoneyQuery, [players[0].token])
+    // not enough money
+    await request(app)
+      .get(`/actionCall?playerToken=${players[0].token}&gameId=${gameId}`)
+      .expect(404)
+
+    const resetMoneyQuery = 'UPDATE Players SET funds=$1 WHERE token=$2'
+    await client.query(resetMoneyQuery, [
+      STARTING_FUNDS_DEFAULT,
+      players[0].token,
+    ])
+
     await request(app)
       .get(
         `/actionRaise?playerToken=${players[1].token}&gameId=${gameId}&amount=5`
@@ -52,6 +84,7 @@ test('Call, correct arguments 1', async () => {
         `/actionRaise?playerToken=${players[0].token}&gameId=${gameId}&amount=300`
       )
       .expect(200)
+
     await request(app)
       .get(`/actionCall?playerToken=${players[1].token}&gameId=${gameId}`)
       .expect(200)
@@ -88,5 +121,103 @@ test('Call, correct arguments 1', async () => {
     await request(app)
       .get(`/actionCall?playerToken=${players[1].token}&gameId=${gameId}`)
       .expect(200)
+  })
+}, 20000)
+
+test('Call, change round when small blind and bigblind folded', async () => {
+  const gameMasterToken = 'CALL2TEST'
+  const gameMasterNick = 'CALL2NICK'
+  const playerToken = 'CALL2TEST2'
+  const playerNick = 'CALL2NICK2'
+  const player2Token = 'CALL2TEST3'
+  const player2Nick = 'CALL2NICK3'
+  const player3Token = 'CALL2TEST4'
+  const player3Nick = 'CALL2NICK4'
+
+  await runRequestWithClient(undefined, async (client) => {
+    const res = await request(app)
+      .get(
+        `/createGame?creatorToken=${gameMasterToken}&nickname=${gameMasterNick}`
+      )
+      .expect(200)
+
+    const key = (res.body as NewGameInfo).gameId
+    await request(app)
+      .get(
+        `/joinGame?playerToken=${playerToken}&nickname=${playerNick}&gameId=${key}`
+      )
+      .expect(200)
+
+    await request(app)
+      .get(
+        `/joinGame?playerToken=${player2Token}&nickname=${player2Nick}&gameId=${key}`
+      )
+      .expect(200)
+
+    await request(app)
+      .get(
+        `/joinGame?playerToken=${player3Token}&nickname=${player3Nick}&gameId=${key}`
+      )
+      .expect(200)
+
+    await request(app)
+      .get(`/startGame?creatorToken=${gameMasterToken}`)
+      .expect(200)
+
+    const gameId =
+      (await getGameIdAndStatus(gameMasterToken, client)).gameId ?? ''
+    const players = await getPlayersInGame(gameId, client)
+
+    await request(app)
+      .get(`/actionCall?playerToken=${players[0].token}&gameId=${gameId}`)
+      .expect(200)
+
+    await request(app)
+      .get(
+        `/actionRaise?playerToken=${players[1].token}&gameId=${gameId}&amount=300`
+      )
+      .expect(200)
+
+    await request(app)
+      .get(`/actionFold?playerToken=${players[2].token}&gameId=${gameId}`)
+      .expect(200)
+
+    await request(app)
+      .get(`/actionFold?playerToken=${players[3].token}&gameId=${gameId}`)
+      .expect(200)
+
+    // round should change, and player 0 should be able to act
+    const getRound = 'SELECT game_round FROM Games WHERE game_id=$1'
+    expect(
+      await (
+        await client.query(getRound, [gameId])
+      ).rows[0].game_round
+    ).toEqual('1')
+
+    await request(app)
+      .get(`/actionCall?playerToken=${players[0].token}&gameId=${gameId}`)
+      .expect(200)
+
+    expect(
+      await (
+        await client.query(getRound, [gameId])
+      ).rows[0].game_round
+    ).toEqual('2')
+
+    await request(app)
+      .get(
+        `/actionRaise?playerToken=${players[1].token}&gameId=${gameId}&amount=301`
+      )
+      .expect(403) // not his turn
+
+    await request(app)
+      .get(
+        `/actionRaise?playerToken=${players[0].token}&gameId=${gameId}&amount=301`
+      )
+      .expect(200) // not his turn
+
+    await request(app)
+      .get(`/actionFold?playerToken=${players[1].token}&gameId=${gameId}`)
+      .expect(201) // game should end
   })
 }, 20000)

src/tests/check.test.ts
@@ -6,14 +6,15 @@ import { getPlayersInGame } from '../utils/commonRequest'
 import './testSuiteTeardown'
 
 test('Check, wrong args', async () => {
-  const gameMasterToken = 'CHECKTEST_INCORRECT_GM'
+  const gameMasterToken = 'CHECKTEST_INC_GM'
   const gameMasterNick = 'CHECK_INC_GM_NICK'
-  const playerToken = 'CHECKTEST_INCORRECT_P1'
+  const playerToken = 'CHECKTEST_INC_P1'
   const playerNick = 'CHECK_INC_P1_NICK'
-  const player2Token = 'CHECKTEST_INCORRECT_P2'
+  const player2Token = 'CHECKTEST_INC_P2'
   const player2Nick = 'CHECK_INC_P2_NICK'
-  const wrongToken = 'TESTCHECK_INCORRECT'
+  const wrongToken = 'TESTCHECK_INC'
   const wrongGameId = 'WRONG_ID'
+  const correctId = '1'
 
   await runRequestWithClient(undefined, async (client) => {
     await request(app).get(`/actionCheck?playerToken=${wrongToken}`).expect(400)
@@ -21,6 +22,10 @@ test('Check, wrong args', async () => {
     await request(app)
       .get(`/actionCheck?playerToken=${wrongToken}&gameId=${wrongGameId}`)
       .expect(400)
+
+    await request(app)
+      .get(`/actionCheck?playerToken=${wrongToken}&gameId=${correctId}`)
+      .expect(402)
 
     const res = await request(app)
       .get(

src/tests/fold.test.ts
@@ -1,17 +1,14 @@
 import { app } from '../app'
 import request from 'supertest'
 import { runRequestWithClient } from '../utils/databaseConnection'
-import { PlayerState, type NewGameInfo } from '../utils/types'
-import {
-  calculateWinner,
-  getGameIdAndStatus,
-  getPlayersInGame,
-} from '../utils/commonRequest'
+import { type NewGameInfo } from '../utils/types'
+import { getPlayersInGame } from '../utils/commonRequest'
 import './testSuiteTeardown'
 
 test('Fold, wrong args', (done) => {
   const wrongToken = 'TESTFOLD_INCORRECT'
   const wrongGameId = 'WRONG_ID'
+  const correctId = '1'
   request(app)
     .get(`/actionFold?playerToken=${wrongToken}`)
     .expect(400)
@@ -20,6 +17,10 @@ test('Fold, wrong args', (done) => {
   request(app)
     .get(`/actionFold?playerToken=${wrongToken}&gameId=${wrongGameId}`)
     .expect(400)
+    .end(done)
+  request(app)
+    .get(`/actionFold?playerToken=${wrongToken}&gameId=${correctId}`)
+    .expect(402)
     .end(done)
 })
 
@@ -75,78 +76,5 @@ test('Fold, correct arguments, wrong turn', async () => {
         await client.query(getRound, [gameId])
       ).rows[0].game_round
     ).toEqual('1') // because last guy has won - no new rounds then.
-  })
-}, 20000)
-
-test('calculate winner', async () => {
-  const gameMasterToken = 'CALCUlATETEST'
-  const gameMasterNick = 'CALCUlATENICK'
-  const playerToken = 'CALCUlATETEST2'
-  const playerNick = 'CALCUlATENICK2'
-  const player2Token = 'CALCUlATETEST3'
-  const player2Nick = 'CALCUlATENICK3'
-
-  await runRequestWithClient(undefined, async (client) => {
-    const res = await request(app)
-      .get(
-        `/createGame?creatorToken=${gameMasterToken}&nickname=${gameMasterNick}`
-      )
-      .expect(200)
-
-    const key = (res.body as NewGameInfo).gameId
-    await request(app)
-      .get(
-        `/joinGame?playerToken=${playerToken}&nickname=${playerNick}&gameId=${key}`
-      )
-      .expect(200)
-
-    await request(app)
-      .get(
-        `/joinGame?playerToken=${player2Token}&nickname=${player2Nick}&gameId=${key}`
-      )
-      .expect(200)
-
-    await request(app)
-      .get(`/startGame?creatorToken=${gameMasterToken}`)
-      .expect(200)
-
-    const gameId =
-      (await getGameIdAndStatus(gameMasterToken, client)).gameId ?? ''
-    const players = await getPlayersInGame(gameId, client)
-
-    await client.query('UPDATE players SET last_action=$1 WHERE game_id=$2', [
-      PlayerState.Called,
-      gameId,
-    ])
-    await request(app)
-      .get(`/actionFold?playerToken=${players[0].token}&gameId=${gameId}`)
-      .expect(200)
-    await client.query('UPDATE players SET CARD1=$1, CARD2=$2 WHERE token=$3', [
-      '01K',
-      '01O',
-      players[1].token,
-    ])
-    await client.query('UPDATE players SET CARD1=$1, CARD2=$2 WHERE token=$3', [
-      '13K',
-      '13O',
-      players[2].token,
-    ])
-    await client.query(
-      'UPDATE games SET CARD1=$1, CARD2=$2, CARD3=$3, CARD4=$4, CARD5=$5  WHERE game_id=$6',
-      ['02K', '03O', '05K', '07O', '08P', gameId]
-    )
-    let calculateResult = await calculateWinner(gameId, client)
-    expect(calculateResult.includes(players[1].token)).toBe(true)
-    expect(calculateResult.includes(players[2].token)).toBe(false)
-    expect(calculateResult.includes(players[0].token)).toBe(false)
-    await client.query('UPDATE players SET CARD1=$1, CARD2=$2 WHERE token=$3', [
-      '01P',
-      '01T',
-      players[2].token,
-    ])
-    calculateResult = await calculateWinner(gameId, client)
-    expect(calculateResult.includes(players[1].token)).toBe(true)
-    expect(calculateResult.includes(players[2].token)).toBe(true)
-    expect(calculateResult.includes(players[0].token)).toBe(false)
   })
 }, 20000)

src/tests/kickPlayer.test.ts
@@ -14,6 +14,10 @@ test('Kick player, wrong args', (done) => {
     .get('/kickPlayer?playerToken=1337'.concat('&creatorToken=1337'))
     .expect(401)
     .end(done)
+  request(app)
+    .get('/kickPlayer?playerToken=1'.concat('&creatorToken=1337'))
+    .expect(402)
+    .end(done)
 })
 
 test('Kick player, correct arguments', async () => {